4d64da35af99a885dd303fda52dc4e4f5361a61b
Paul Deen <pauldeen@gmx.li>
2026-05-25 20:22:58 +0200

    Added TestContainers (PostgreSQL)

--- src/test/java/org/pauldeen/playground/PlaygroundApplicationTests.java
+++ src/test/java/org/pauldeen/playground/PlaygroundApplicationTests.java
@@ -2,9 +2,18 @@ package org.pauldeen.playground;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
+@Testcontainers
 class PlaygroundApplicationTests {
+
+	@Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
