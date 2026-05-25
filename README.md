c5359114f27f8594452e67dc590d33fe1d3d06a0
Paul Deen <pauldeen@gmx.li>
2026-05-25 11:02:39 +0200

    Initial creation of Spring Boot project

---
+++ README.md
@@ -0,0 +1,3 @@
+To open the Swagger UI in a browser, go to: http://localhost:8080/swagger-ui/index.html
+
+To open the Kafka 
---
+++ src/test/java/org/pauldeen/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pauldeen.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
