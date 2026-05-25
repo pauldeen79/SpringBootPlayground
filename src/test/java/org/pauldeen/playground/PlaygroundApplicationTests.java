a1f2c877c3f634bfbe166dd3ddd768c3543c9b35
Paul Deen <pauldeen@gmx.li>
2026-05-25 20:28:15 +0200

    Added Kafka test containers

--- src/test/java/org/pauldeen/playground/PlaygroundApplicationTests.java
+++ src/test/java/org/pauldeen/playground/PlaygroundApplicationTests.java
@@ -5,15 +5,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 @Testcontainers
 class PlaygroundApplicationTests {
 
-	@Container
+    // 1. PostgreSQL Container Setup
+    @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    // 2. Kafka Container Setup
+	@Container
+	@ServiceConnection
+	static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("apache/kafka:3.7.0"));
 
 	@Test
 	void contextLoads() {
