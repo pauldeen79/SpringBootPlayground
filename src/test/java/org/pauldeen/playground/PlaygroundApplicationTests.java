a15299feb9cb3634b577642b48a00fcf5469db87
Paul Deen <pauldeen@gmx.li>
2026-09-18 14:10:21 +0200

    let's upgrade some stuff

--- src/test/java/org/pauldeen/playground/PlaygroundApplicationTests.java
+++ src/test/java/org/pauldeen/playground/PlaygroundApplicationTests.java
@@ -3,9 +3,10 @@ package org.pauldeen.playground;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.PostgreSQLContainer;
+
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.junit.jupiter.Container;
 
@@ -14,14 +15,15 @@ import org.testcontainers.junit.jupiter.Container;
 class PlaygroundApplicationTests {
 
     // 1. PostgreSQL Container Setup
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer postgres =
+		new PostgreSQLContainer("postgres:18-alpine");
 
     // 2. Kafka Container Setup
 	@Container
 	@ServiceConnection
-	static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("apache/kafka:3.7.0"));
+	static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("apache/kafka:4.3.1"));
 
 	@Test
 	void contextLoads() {
